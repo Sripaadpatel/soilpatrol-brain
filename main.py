f31c00b7ddfbe727d6e3c90dcd3f98a9858d91fd
sripaadpatel <sripaad50724@gmail.com>
2026-04-09 11:21:25 +0530

    Bug fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import hashlib  # <-- MISSING THIS ONE
+import json     # <-- MISSING THIS ONE
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
